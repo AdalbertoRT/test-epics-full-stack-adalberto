1f4cb030dbe04a608b637d72dfd0e442f6fefb5e
Adalberto R. Teixeira <adalbertoart@gmail.com>
2022-04-26 01:06:04 -0300

    Update 1.1.2

--- resources/js/ReactJS/helpers/formatDate.js
+++ resources/js/ReactJS/helpers/formatDate.js
@@ -1,20 +1,26 @@
 export function formatDate(date, format) {
     const newDate = new Date(date);
     const map = {
-        mm: ((newDate.getMonth() + 1) < 10) ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1,
-        dd: newDate.getDate(),
+        mm:
+            newDate.getMonth() + 1 < 10
+                ? "0" + (newDate.getMonth() + 1)
+                : newDate.getMonth() + 1,
+        dd:
+            newDate.getDate() + 1 < 10
+                ? "0" + (newDate.getDate() + 1)
+                : newDate.getDate() + 1,
         aa: newDate.getFullYear().toString().slice(-2),
-        yyyy: newDate.getFullYear()
-    }
+        yyyy: newDate.getFullYear(),
+    };
 
-    return format.replace(/mm|dd|aa|yyyy/gi, matched => map[matched])
+    return format.replace(/mm|dd|aa|yyyy/gi, (matched) => map[matched]);
 }
 
-export function formatDateString(date){
+export function formatDateString(date) {
     const newDate = new Date(date);
     const string = newDate.toDateString();
     const array = string.toString().split(" ");
     array.shift();
-    const text = array.join(" ")
-    return text
+    const text = array.join(" ");
+    return text;
 }
